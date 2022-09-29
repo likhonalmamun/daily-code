6a5f0f2911fb70edb9488fbda7314ef13f24e608
likhonalmamun <alikhon055@gmail.com>
2022-09-29 12:55:34 +0600

    added summery section

--- src/Components/Summery/Summery.js
+++ src/Components/Summery/Summery.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import "../Summery/Summery.css";
 const Summery = () => {
+
+ let [breakTime , setBreakTime] = useState('')
+
+
+
   return (
     <div className="summery">
       <div className="user">
-        <img src="" alt="" />
+        <img src="user.jpg" alt="" />
         <h2>Likhon Al Mamun</h2>
       </div>
       <div className="user-info">
@@ -23,17 +29,17 @@ const Summery = () => {
       </div>
     <h2>  Add a break</h2>
       <div className="break">
-        <button>5m</button>
+        <button >5m</button>
         <button>10m</button>
         <button>20m</button>
         <button>30m</button>
       </div>
       <h2>Task Details</h2>
       <div className="task-info">
-        <h2>Task Duration : </h2> <h3></h3>
+        <h2>Task Duration : <span>   </span> </h2>
       </div>
       <div className="break-info">
-        <h2>Break Time :</h2> <h3></h3>
+        <h2>Break Time : <span>  </span> </h2>
       </div>
     </div>
   );
